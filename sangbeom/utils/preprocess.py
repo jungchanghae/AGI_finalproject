aa51eddb77b297813fbc37be441ade13c4760edc
jungchanghae <changhae08@korea.ac.kr>
2023-11-19 14:28:09 +0000

    bug fix

--- sangbeom/utils/preprocess.py
+++ sangbeom/utils/preprocess.py
@@ -19,19 +19,23 @@ def create_template(class_labels, gold_label, candidate_num):
 
     random.shuffle(candidate_class)
     gold_index = candidate_class.index(gold_label)
-    template = (f"Choose your answer between "
-                # f"1={candidate_class[0]}, 2={candidate_class[1]}, 3={candidate_class[2]}, 4={candidate_class[3]}"
-                # f". Answer the question with integer form."
-                f"'{candidate_class[0]}', '{candidate_class[1]}', '{candidate_class[2]}'"
-                f", '{candidate_class[3]}' that best describes the contexts."
-                )
-
+    # template = (f"Choose your answer between "
+    #             # f"1={candidate_class[0]}, 2={candidate_class[1]}, 3={candidate_class[2]}, 4={candidate_class[3]}"
+    #             # f". Answer the question with integer form."
+    #             f"'{candidate_class[0]}', '{candidate_class[1]}', '{candidate_class[2]}'"
+    #             f", '{candidate_class[3]}' that best describes the contexts."
+    #             )
+    template = "Choose your answer between "
+    for i in range(candidate_num-1):
+        template += f"'{candidate_class[i]}', "
+    template += f"'{candidate_class[-1]}' that best describes the contexts."
+    
     return template, gold_index + 1
 
 
 def evaluate_answer(pred_label, gold_label, matched_num, sub_matched_result):
     # Exact Accuracy
-    gold_label = gold_label.replace("_", " ")
+    gold_label = gold_label.lower().replace("_", " ")
     pred_label = pred_label.replace("_", " ")
     # print(gold_label, pred_label)
     if gold_label in pred_label:
